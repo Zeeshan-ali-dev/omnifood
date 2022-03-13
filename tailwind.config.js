94be881d5a5dfa4826a04eca7286b69544006559
dev-shani <zeeshan.ali60767@gmail.com>
2022-03-14 00:39:25 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ['./dist/*.{html, js}'],
+  theme: {
+    extend: {
+      colors: {
+        'hero': '#fdf2e9',
+        'golden': '#e67e22',
+        'golden-dim': '#cf711f',
+        'golden-dark': '#45260a'
+      },
+    },
+    container: {
+      center: true,
+    }
+  },
+  plugins: [],
+}
